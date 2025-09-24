c7aeb206e60186d53c3712240256616e47f97215
vishnu vardhan <vishnuvvr3@gmail.com>
2025-09-24 18:34:00 +0530

    3r commit

--- Practice1.java
+++ Practice1.java
@@ -2,6 +2,6 @@ public class Practice1 {
 
     public void firstMethod(){
         System.out.println("sahithi");
-        System.out.println("Vishnu loves athidhi");
+        System.out.println("Vishnu loves sahithi");
     }
 }
